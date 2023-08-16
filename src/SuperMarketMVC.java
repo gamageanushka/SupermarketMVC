c7b903e416532a380269f1a11ca751cee85287cc
gamageanushka <gamageanushkagamage@gmail.com>
2023-08-16 06:09:01 +0530

    Create User Interface

---
+++ src/SuperMarketMVC.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supermarket;
+
+import supermarket.view.CustomerView;
+import supermarket.view.InterfaceView;
+import supermarket.view.ItemView;
+import supermarket.view.OrderView;
+
+/**
+ *
+ * @author user
+ */
+public class SuperMarketMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new InterfaceView().setVisible(true);
+    }
+    
+}
